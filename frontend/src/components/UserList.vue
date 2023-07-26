c3f955bc4f1a8228d80410dff71cdb31f4f9dc69
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-26 19:37:52 -0400

    New: Show user list with their names

---
+++ frontend/src/components/UserList.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data: () => ({
+    users: null,
+  }),
+
+  created() {
+    this.getUsers();
+  },
+
+  methods: {
+    async getUsers() {
+      const url = "http://localhost:8001";
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network error");
+        }
+        const data = await response.json();
+        this.users = data.users;
+      } catch (error) {
+        alert("Error getting users: " + error);
+      }
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <h1>Users</h1>
+    <template v-if="users">
+      <ul v-if="users">
+        <li v-for="user in users" :key="user.id">
+          {{ user.name }}
+        </li>
+      </ul>
+    </template>
+    <template v-else>
+      <h3>No users found</h3>
+    </template>
+  </div>
+</template>
