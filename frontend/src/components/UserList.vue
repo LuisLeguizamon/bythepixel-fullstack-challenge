01c72c997a25fb893f77f2afa9eb258d826878c3
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-28 18:26:46 -0400

    Style: New UI for user list with weather info

--- frontend/src/components/UserList.vue
+++ frontend/src/components/UserList.vue
@@ -28,14 +28,48 @@ export default {
 </script>
 <template>
   <div>
-    <h1>Users</h1>
     <template v-if="users">
-      <ul v-if="users">
-        <li v-for="user in users" :key="user.id">
-          {{ user.user_name }} - Location Temperature: {{ user.temperature_in_kelvin }}
-          <router-link :to="'/weather/' + user.id">Show More</router-link>
-        </li>
-      </ul>
+      <div
+        v-for="user in users"
+        :key="user.id"
+        class="bg-white opacity-70 my-5 pt-5 pb-3 pl-10 grid grid-cols-2 rounded-lg overflow-hidden shadow-lg"
+      >
+        <div>
+          <h1 class="font-medium text-2xl">
+            {{ user.user_name }}
+          </h1>
+          <p class="my-5 text-slate-500 text-medium">
+            <span class="font-medium">Location</span> {{ user.location }}
+          </p>
+          <p class="my-5">
+            <router-link
+              class="text-sm text-sky-600 hover:text-sky-800 transition ease-in-out delay-100"
+              :to="'/weather/' + user.id"
+            >
+              More Info
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </router-link>
+          </p>
+        </div>
+        <div>
+          <p class="text-slate-500 text-4xl font-medium">
+            {{ user.temperature_in_kelvin }}
+          </p>
+        </div>
+      </div>
     </template>
     <template v-else>
       <h3>No users found</h3>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import UserList from "@/components/UserList.vue";
 </script>
 
 <template>
-  <main>
+  <main class="bg-gradient-to-r from-blue-100 to-cyan-100 flex justify-center">
     <user-list></user-list>
   </main>
 </template>
