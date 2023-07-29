b4434b66e7565320b45630afd4de0534b63f6c7d
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-28 23:45:10 -0400

    Adjust: Remove unuseful code

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,14 +14,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'all systems are a go',
-        'users' => \App\Models\User::all(),
-    ]);
-});
-
 Route::get('/users/weather-info', [UserWeatherInfoController::class, 'index'])->name('weather.index');
 Route::get('/users/{user}/weather-info', [UserWeatherInfoController::class, 'show'])->name('weather.show');
-Route::get('/weather/{user}', [WeatherController::class, 'show'])->name('weather.show');
