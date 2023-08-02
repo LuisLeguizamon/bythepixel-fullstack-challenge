3c21c619a8bc9a5dcb0fa8b13574d8a1c79a3e11
Luis Leguizamón <lleguizamonl@gmail.com>
2023-08-02 09:12:52 -0400

    Adjust: Rename test for weather info index

--- api/tests/Feature/WeatherInfo/WeatherInfoIndexTest.php
+++ api/tests/Feature/WeatherInfo/WeatherInfoIndexTest.php
@@ -6,11 +6,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class WeatherInfoTest extends TestCase
+class WeatherInfoIndexTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_weather_info_screen_can_be_rendered(): void
+    public function test_index_returns_a_successful_response()
     {
         $response = $this->get('/users/weather-info');
 
